8510b6c8207b09c4ed4419958efb3892733ebc79
Sxpkait <684259035@webmail.npru.ac.th>
2026-03-11 11:29:00 +0700

    UPDATE TABLE

--- select_injection_solution.php
+++ select_injection_solution.php
@@ -1,19 +1,49 @@
 <?php
 require "connect.php";
 
-//$n = "1". or '1=1";
-$cid = $_GET["CustomerID"];
-$sql = "SELECT * FROM customer where CustomerID = :customerID";
-$stmt = $conn->prepare($sql);
+if (isset($_GET["CustomerID"])) {
+    $cid = $_GET["CustomerID"];
 
-$stmt->bindParam(':customerID', $cid);
+    $sql = "SELECT customer.CustomerID, customer.Name, customer.Email, customer.OutstandingDebt, country.CountryName 
+            FROM customer 
+            INNER JOIN country ON customer.CountryCode = country.CountryCode 
+            WHERE customer.CustomerID = :customerID";
 
-$stmt->execute();
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':customerID', $cid);
+    $stmt->execute();
 
-$stmt->setFetchMode(PDO::FETCH_ASSOC);
+    if ($stmt->rowCount() > 0) {
 
-while ($row = $stmt->fetch()) {
-    echo $row['CustomerID'] . ' ' . $row['Name'] . "<br/>";
+        echo "<br><br>";
+        echo "<table border='1' cellpadding='5' cellspacing='0'>";
+
+        echo "<tr>
+                <th>รหัสลูกค้า</th>
+                <th>ชื่อ-นามสกุล</th>
+                <th>อีเมล</th>
+                <th>ประเทศ</th>
+                <th>ยอดหนี้ค้างชำระ (บาท)</th>
+              </tr>";
+
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        while ($row = $stmt->fetch()) {
+            echo "<tr>";
+            echo "<td>" . $row['CustomerID'] . "</td>";
+            echo "<td>" . $row['Name'] . "</td>";
+            echo "<td>" . $row['Email'] . "</td>";
+            echo "<td>" . $row['CountryName'] . "</td>";
+            echo "<td>" . number_format($row['OutstandingDebt'], 2) . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "<br><br>ไม่พบข้อมูลของลูกค้ารหัส: " . htmlspecialchars($cid);
+    }
+} else {
+    echo "<br>กรุณาระบุ CustomerID ใน URL (เช่น ?CustomerID=Cus001)";
 }
 
 $conn = null;
